45e4f97baf6714305568ab29486b779b0270643f
Michael Delfino <megabeano@gmail.com>
2013-07-06 13:47:00 -0400

    Add github ribbon to Contact page

--- contact.php
+++ contact.php
@@ -68,7 +68,6 @@
   </head>
 
   <body>
-
     <!-- NAVBAR
     ================================================== -->
     <div class="navbar-wrapper">
@@ -122,6 +121,8 @@
 				?>
 
     </div> <!-- /container -->
+
+    <a href="https://github.com/madelfino"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap-transition.js"></script>
