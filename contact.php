e3dc125757af684c0d0462c54d730d6fc5bac2e3
Michael Delfino <megabeano@gmail.com>
2013-07-06 10:19:26 -0400

    Recreate Contact page using bootstrap

--- contact.php
+++ contact.php
@@ -1,18 +1,53 @@
-<html>
-	<head>
-		<title>Michael A. Delfino</title>
-		<link rel="stylesheet" type="text/css" href="delfino.css" />
-	</head>
-	<body>
-		<center>
-		<div id="nav">
-			<a href="http://www.michaeldelfino.com" class="main"><font color="#000000">michael</font><font color="#F57218">delfino</font></a><br />
-			<a href="index.html">home</a>&nbsp;|&nbsp;
-			<a href="about.html">about</a>&nbsp;|&nbsp;
-			<a href="pictures.html">pictures</a>&nbsp;|&nbsp;
-			<a href="contact.html" class="selected">contact</a>
-		</div>
-		<div id="content">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>About Michael Delfino</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="http://www.michaeldelfino.com">Michael Delfino</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="projects.html">Projects</a></li>
+              <li class="active"><a href="contact.html">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
 			<?php
 					$to = "megabeano@gmail.com";
 					$name = $_POST['name'];
@@ -32,7 +67,21 @@
  					}
 				?>
 
-		</div>
-		</center>
-	</body>
+    </div> <!-- /container -->
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
 </html>
